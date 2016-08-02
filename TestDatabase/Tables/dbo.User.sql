90a6800a4f39871b3a1505abce583a58de57c20a
rchandrikadev <rchandrika@ipipeline.com>
2016-08-02 15:01:04 -0400

    Test

--- TestDatabase/Tables/dbo.User.sql
+++ TestDatabase/Tables/dbo.User.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[User]
 (
-[Name] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Email] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Name] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Email] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
